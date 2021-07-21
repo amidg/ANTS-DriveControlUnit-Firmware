e5d2c855a976c8506c6826943626bb8f453f7ddc
Dmitrii Gusev <amidg4x4dima@gmail.com>
2021-07-21 12:03:55 -0700

    GIGAVAC ROS added

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Int16.h"
+#include "std_msgs/Int32.h"
 
 //MOTOR CONTROL
 Adafruit_MCP23017 motorControl;
@@ -121,6 +122,7 @@ void FrontRightROS(const std_msgs::Int16& msg1); //motor 1
 void FrontLeftROS(const std_msgs::Int16& msg2); //motor 2
 void RearLeftROS(const std_msgs::Int16& msg3); //motor 3
 void RearRightROS(const std_msgs::Int16& msg4); //motor 4
+void unlockPowerToMotors(const std_msgs::Int16& msg5); //GIGAVAC contactor
 
 int FrontRightMotor1speed;
 int FrontLeftMotor2speed;
@@ -131,6 +133,24 @@ ros::Subscriber<std_msgs::Int16> FrontRightSpeed("/dcu1/motor1/cmd", FrontRightR
 ros::Subscriber<std_msgs::Int16> FrontLeftSpeed("/dcu1/motor2/cmd", FrontLeftROS); //Front Left Wheel, motor 2
 ros::Subscriber<std_msgs::Int16> RearLeftSpeed("/dcu1/motor3/cmd", RearLeftROS); //Rear Left wheel, motor 3
 ros::Subscriber<std_msgs::Int16> RearRightSpeed("/dcu1/motor4/cmd", RearRightROS); //Rear Right Wheel, motor 4
+
+//contactor power
+ros::Subscriber<std_msgs::Int16> PowerLock("/dcu1/contactor", unlockPowerToMotors);
+std_msgs::String powerLocker_msg;
+ros::Publisher motor_power("gigavac_feedback", &powerLocker_msg);
+
+//ROS Encoder Control:
+std_msgs::Int32 FrontRightEncMsg; //Front Right, motor 1 encoder message to ROS
+ros::Publisher FrontRightEncPublish("/dcu1/motor1/enc", &FrontRightEncMsg);
+
+std_msgs::Int32 FrontLeftEncMsg; //Front Left, motor 2 encoder message to ROS
+ros::Publisher FrontLeftEncPublish("/dcu1/motor2/enc", &FrontLeftEncMsg);
+
+std_msgs::Int32 RearLeftEncMsg; //Rear Left, motor 3 encoder message to ROS
+ros::Publisher RearLeftEncPublish("/dcu1/motor3/enc", &RearLeftEncMsg);
+
+std_msgs::Int32 RearRightEncMsg; //Rear Right, motor 4 encoder message to ROS
+ros::Publisher RearRightEncPublish("/dcu1/motor4/enc", &RearRightEncMsg);
 
 //MAIN FUNCTION ===============================================================================
 void setup()
@@ -196,7 +216,20 @@ void setup()
   Serial.print("IP = ");
   Serial.println(DCU1.getHardware()->getLocalIP());
 
-  // Start to be polite
+  //motor subs -> read DCU power from ROS and apply to motors
+  DCU1.subscribe(FrontRightSpeed); //motor 1
+  DCU1.subscribe(FrontLeftSpeed); //motor 2
+  DCU1.subscribe(RearLeftSpeed); //motor 3
+  DCU1.subscribe(RearRightSpeed); //motor 4
+
+  //motor publishing -> read encoders on DCU side and publish them to ROS
+
+  //contactor ROS -> subscribe to unlock, publish to let PC know gigavac is engaged
+  DCU1.subscribe(PowerLock); //contactor subscriber
+  DCU1.advertise(motor_power); //contactor feedback publisher
+
+
+  //Start the ROS node on DCUx
   DCU1.advertise(chatter);
 }
 
@@ -208,8 +241,11 @@ void loop()
     Serial.println("ERROR: NO ROS CONNECTION");
   }
 
+  // topic publishers
+  motor_power.publish( &powerLocker_msg ); //power locker, str_msg.data is generated based on the input from the ROS power locker 
+
   //enable GIGAVAC
-  digitalWrite(GIGAVACENABLE, HIGH);
+  unlockPowerToMotors(const std_msgs::Int16& msg5);
 
   //run motors based on ROS -> single DCU 4ch operation
   moveMotorsBasedOnROS(); 
@@ -279,6 +315,8 @@ void RearRightROS(const std_msgs::Int16& msg4) { //motor 4 data from ROS to moto
 
 void moveMotorsBasedOnROS() {
   //make sure to stop motors if there is 0 velocity command from ROS
+  digitalWrite(GIGAVACENABLE, HIGH);
+
   if (FrontRightMotor1speed == 0) {
     FrontRightMotor.stop(&motorControl);
   } else {
@@ -301,5 +339,18 @@ void moveMotorsBasedOnROS() {
     RearRightMotor.stop(&motorControl);
   } else {
     RearRightMotor.go(&motorControl, RearRightMotor4speed);
+  }
+}
+
+void unlockPowerToMotors(const std_msgs::Int16& msg5) {
+  //unlock power to motors based on ROS command
+  if(msg5.data == 0) {
+    digitalWrite(GIGAVACENABLE, HIGH); //HIGH turns it off
+    powerLocker_msg.data = "POWER IS OFF";
+  }
+
+  else if (msg5.data == 1) {
+    digitalWrite(GIGAVACENABLE, LOW); //LOW turns it on
+    powerLocker_msg.data = "POWER IS ON";
   }
 }
