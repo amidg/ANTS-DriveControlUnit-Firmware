b1c593ee7ea4539924b2bdb78ae6d94f26e8f771
Dmitrii Gusev <amidg4x4dima@gmail.com>
2021-08-01 20:09:12 -0700

    replaced feedback on Serial port with actual speed

--- src/main.cpp
+++ src/main.cpp
@@ -127,21 +127,21 @@ void loop()
 // ADDITIONAL FUNCTIONS ================================================================================
 void FrontRightROS(const std_msgs::Float32& msg1) { //motor 1 data from ROS to motor control
     FrontRightMotor1speed = (-1)*25*(MAXPOWER/100)*(msg1.data); //-1 is required because of FET polarity VS BJS polarity
-    Serial.println(msg1.data);
+    Serial.println(FrontRightMotor1speed);
 }
 
 void FrontLeftROS(const std_msgs::Float32& msg2) { //motor 2 data from ROS to motor control
     FrontLeftMotor2speed = (-1)*255*(MAXPOWER/100)*(msg2.data);
-    Serial.println(msg2.data);
+    Serial.println(FrontLeftMotor2speed);
 }
 void RearLeftROS(const std_msgs::Float32& msg3) { //motor 3 data from ROS to motor control
     RearLeftMotor3speed = (-1)*255*(MAXPOWER/100)*(msg3.data);
-    Serial.println(msg3.data);
+    Serial.println(RearLeftMotor3speed);
 } 
 
 void RearRightROS(const std_msgs::Float32& msg4) { //motor 4 data from ROS to motor control
     RearRightMotor4speed = (-1)*255*(MAXPOWER/100)*(msg4.data);
-    Serial.println(msg4.data);
+    Serial.println(RearRightMotor4speed);
 }
 
 void moveMotorsBasedOnROS() {
